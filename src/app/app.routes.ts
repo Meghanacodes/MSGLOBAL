df902a8e67b373c7521c378aa4d681f3e8d312a4
Meghana <rmeghana53@gmail.com>
2025-07-07 10:43:36 +0530

    fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { B2bMetrics } from './b2b-metrics/b2b-metrics';
 import { HighTrafficLocations } from './high-traffic-locations/high-traffic-locations';
 import { Footer } from './footer/footer';
 import { SolutionsSection } from './solutions-section/solutions-section';
+import { ContactForm } from './contact-form/contact-form';
 
 export const routes: Routes = [
   { path: 'custom-solutions', component: CustomSolutions },
   { path: 'b2b-metrics', component: B2bMetrics },
   { path: 'services', component: SolutionsSection },
   { path: 'locations', component: HighTrafficLocations },
+  { path: 'contact', component: ContactForm},
   { path: 'footer', component: Footer },
 ];
