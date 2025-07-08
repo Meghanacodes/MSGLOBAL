6b94f24b8ad9d0ef0edc6da2b129782796e5f301
Meghana <rmeghana53@gmail.com>
2025-07-08 11:42:10 +0530

    custom-solution component added

--- src/app/custom-solutions/custom-solutions.ts
+++ src/app/custom-solutions/custom-solutions.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-custom-solutions',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './custom-solutions.html',
   styleUrl: './custom-solutions.css'
 })
