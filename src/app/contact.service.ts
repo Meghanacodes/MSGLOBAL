067d9aea7cda323b95effd8c22622eb383d88e77
Meghana <rmeghana53@gmail.com>
2025-07-10 19:15:39 +0530

    contact keys updated

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,9 +8,9 @@ export class ContactService {
   constructor() {}
 
   sendEmail(formData: any): Promise<any> {
-    const SERVICE_ID = 'service_aagcmok';
-    const TEMPLATE_ID = 'template_hlfy2en';
-    const PUBLIC_KEY = 'RWFLqy5caAhcWWC3j';
+    const SERVICE_ID = 'service_81qipql';
+    const TEMPLATE_ID = 'u-Qos7PjA9DfOTk7YUowS';
+    const PUBLIC_KEY = 'Je9cS0ySWv7yM8ur-';
 
     return emailjs.send(SERVICE_ID, TEMPLATE_ID, formData, PUBLIC_KEY);
   }
